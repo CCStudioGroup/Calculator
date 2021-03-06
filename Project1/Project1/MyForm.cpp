3d17596580a2f218ac62d113b105c3c8de25dafa
JohnsonKHOO <57935850+JohnsonKHOO@users.noreply.github.com>
2021-03-06 16:18:41 +0800

    Add files via upload
    Final

--- Project1/Project1/MyForm.cpp
+++ Project1/Project1/MyForm.cpp
@@ -8,6 +8,7 @@ void Main(array<String^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	Project1::MyForm form;//Project1是文件名，尽量跟我的一样,MyForm是窗口名(.h&.cpp)
+	Project1::MyForm form;
 	Application::Run(% form);
 }
+
